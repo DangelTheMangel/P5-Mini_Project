9228b9d000af464168de6b23a680c81ad02c1ed8
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-11-02 14:24:36 +0100

    Made it so it print out the current postion and give the height value on the curretn postion

--- ht/Mediap pipe 2.py
+++ ht/Mediap pipe 2.py
@@ -1,16 +1,28 @@
 import cv2
 import mediapipe as mp
+import os
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-
+folder = os.path.join(os.getcwd(),"HeighMap")
+heightmaps = os.listdir(folder)
+print(os.path.join(folder,heightmaps[0]))
+heighMap = cv2.imread(os.path.join(folder,heightmaps[0]), cv2.IMREAD_GRAYSCALE)
+hmPreview = None
+resieHatMap = False
 while True:
     success, image = cap.read()
+    image = cv2.flip(image, 1)
+    if resieHatMap == False and success == True:
+        heighMap = cv2.resize(heighMap, (image.shape[1], image.shape[0]))
+        resieHatMap = True
+
+    hmPreview = heighMap.copy()
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(imageRGB)
-
+    #lastPos = [0,0]
         # checking whether a hand is detected
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks: # working with each hand
@@ -18,19 +30,21 @@ while True:
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 if id == 8 :
-                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-
-                    for (x, y, w, h) in id:
-                        x1 = x
-                        x2 = x + w
-                        y1 = y
-                        y2 = y + h
-                    print ("diaginal point 1 (x1,y1) = ({},{})".format(x1, y1))
-                    print ("diaginal point 2 (x2,y2) = ({},{})".format(x2, y2))
+                    #lastPos = [cx,cy]
+                    try:
+                        heightValue = int(heighMap[cx,cy])
+                        cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                        cv2.circle(hmPreview, (cx, cy), 25, (heightValue, heightValue, heightValue), cv2.FILLED)
+                        print("cx: " + str(cx)+ " cy: " + str(cy) + " color value: " + str(heighMap[cx,cy]))
+                    except:
+                        print("An exception occurred")
+                    #print ("diaginal point 1 (x1,y1) = ({},{})".format(x1, y1))
+                    #print ("diaginal point 2 (x2,y2) = ({},{})".format(x2, y2))
 
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
     
 
 
-    cv2.imshow("Output", cv2.flip(image, 1))
+    cv2.imshow("Input", image)
+    cv2.imshow("Output", hmPreview)
     cv2.waitKey(1)
