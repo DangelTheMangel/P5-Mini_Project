6159651f72ac43dc509094630823fbde54286b2d
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2023-11-02 11:25:56 +0100

    Hand

--- Mediap pipe 2.py
+++ Mediap pipe 2.py
@@ -20,7 +20,17 @@ while True:
                 if id == 8 :
                     cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
+                    for (x, y, w, h) in id:
+                        x1 = x
+                        x2 = x + w
+                        y1 = y
+                        y2 = y + h
+                    print ("diaginal point 1 (x1,y1) = ({},{})".format(x1, y1))
+                    print ("diaginal point 2 (x2,y2) = ({},{})".format(x2, y2))
+
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+    
+
 
     cv2.imshow("Output", cv2.flip(image, 1))
     cv2.waitKey(1)
