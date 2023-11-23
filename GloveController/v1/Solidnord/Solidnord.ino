346c99dd3f04cb66c50f6ce48e325a6d99438836
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2023-11-23 14:43:36 +0100

    Update Solidnord.ino

--- GloveController/v1/Solidnord/Solidnord.ino
+++ GloveController/v1/Solidnord/Solidnord.ino
@@ -7,9 +7,9 @@ int MoPin = 2;  // Vibration motor connected to digital pin 2
 int analogpin = 3;
 int amplitude = 1.5;
 int frequency = 50;
-int angularFrequency =20;
+int angularFrequency = 20;
 int afDivider = 1000;
-int minimumDelay = 1000;
+int minimumDelay = 100;
 float pi = 3.14;
 char sepperatrorChar = ';';
 char endchar = '\n';
@@ -37,11 +37,25 @@ const unsigned int MAX_MESSAGE_LENGTH = 20;
 int CycleCounter = 0;
 //https://www.programmingelectronics.com/serial-read/
 void loop() {
+  readData();
+  if (testsignal) {
+    //float signal = amplitude * (sin(CycleCounter*angularFrequency/afDivider)+1);
+    //float signal = amplitude * (sin((CycleCounter*2*pi*frequency+phase)+1));
+    float signal = amplitude * (sin(CycleCounter * angularFrequency / afDivider) + phase);
+    Serial.println(signal);
+    analogWrite(analogpin, signal);
+    CycleCounter += 1;
+  }
+}
+
+//Create a place to hold the incoming message
+char message[MAX_MESSAGE_LENGTH];
+
+void readData() {
   //Check to see if anything is available in the serial receive buffer
   while (Serial.available() > 0) {
-    //Create a place to hold the incoming message
-    static char message[MAX_MESSAGE_LENGTH];
     static unsigned int message_pos = 0;
+    //message = char[MAX_MESSAGE_LENGTH]
     //Read the next available byte in the serial receive buffer
     char inByte = Serial.read();
     //Message coming in (check not terminating character) and guard for over message size
@@ -52,30 +66,31 @@ void loop() {
     }
     //Full message received...
     else {
-      String input = " ";
-      for (char c : message) {
-        if (c == sepperatrorChar) {
-          break;
-        }
-        if(c == 'b'){
-          testsignal = !testsignal;
-          testVibration();
-        }
-        input += c;
-      }
-      in_Signal = input.toInt();
-      Serial.println(in_Signal);
-      analogWrite(analogpin, in_Signal);
+      interpretData();
       message_pos = 0;
     }
   }
-  if(testsignal){
-  //float signal = amplitude * (sin(CycleCounter*angularFrequency/afDivider)+1);
-  //float signal = amplitude * (sin((CycleCounter*2*pi*frequency+phase)+1));
-  float signal = amplitude * (sin(CycleCounter*angularFrequency/afDivider)+phase);
-  Serial.println(signal);
-  analogWrite(analogpin, signal);
-  CycleCounter += 1;
-
-  }
 }
+
+void interpretData() {
+  String input = " ";
+  for (char c : message) {
+    if (c == endchar) {
+      break;
+    }
+    if (c == 'b') {
+      testsignal = !testsignal;
+      testVibration();
+    }
+    input += c;
+    //c = '\0';
+  }
+  in_Signal = input.toFloat();
+  //Serial.println(in_Signal);
+  analogWrite(analogpin, in_Signal);
+  delay(minimumDelay);
+  analogWrite(analogpin, 0);
+  Serial.println(in_Signal);
+  memset(message, '\0', MAX_MESSAGE_LENGTH);
+}
+
