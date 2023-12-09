b5c0a73691e54a370517dbf13d56f9812500c2dd
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-12-09 14:28:49 +0100

    added comments!!1

--- GloveController/v1/Solidnord/Solidnord.ino
+++ GloveController/v1/Solidnord/Solidnord.ino
@@ -1,31 +1,33 @@
-////grøn er minus
-////Blå er plus
-////Lilla er R
-////ORange er ground
-//// pink og sort er + - for den bevælgige del
-int MoPin = 2;  // Vibration motor connected to digital pin 2
-int analogpin = 3;
-int amplitude = 1.5;
-int frequency = 50;
-int angularFrequency = 20;
-int afDivider = 1000;
-int minimumDelay = 10;
-float pi = 3.14;
-char sepperatrorChar = ';';
-char endchar = '\n';
-int in_Signal;
-int phase = 1;
-bool testsignal = false;
-bool readyToRecvieSignal = false;
+//###[Pins]###
+int MoPin = 2;        // Digital pin for the vibration motor
+int analogpin = 3;    // Analog pin for vibration control
+//###[The signal]###
+int in_Signal; // the signal rescivede from the other script
+bool readyToRecvieSignal = false; //Boolean that say if the script is ready to recvive a new signal
+//###[Bumps settings]###
+int minimumDelay = 10; //The minimum delay between bumps
+bool useBumps = true;  // Use bumps in signal or continuous signal
+//###[The test signal]###
+bool testsignal = false; //If the test signa lshould be play
+int CycleCounter = 0; //The time to contine the test signal
+int amplitude = 1.5;  // Amplitude of the test signal
+int frequency = 50;   // Frequency of the test signal
+int phase = 1; // phase for test signal
+int angularFrequency = 20; // The angaularFrequency of test signal
+int afDivider = 1000; //devider for the test signal
+float pi = 3.14; //pi... but not so accorate
+
+//Run when the ardino starts. It set up pins and serial and then start a test vibarations 
 void setup() {
   pinMode(MoPin, OUTPUT);
   pinMode(analogpin, OUTPUT);
   Serial.begin(9600);
   Serial.println("Serial Ready to take commands");
-  testVibration();
+  startVibaration();
 }
 
-void testVibration() {
+//This is a startvibaration that runs when the ardino runs starts and it just vibrate and then the ardino is ready to get signals
+void startVibaration() {
   Serial.println("Test signal ready");
   digitalWrite(MoPin, HIGH);
   delay(1000);
@@ -34,42 +36,54 @@ void testVibration() {
   Serial.println("Test signal done");
   readyToRecvieSignal = true;
 }
-const unsigned int MAX_MESSAGE_LENGTH = 20;
-int CycleCounter = 0;
-//https://www.programmingelectronics.com/serial-read/
+
+//if testsignal are true it send out testsignals else it look at the incomming data and create bumps
 void loop() {
-  readData();
   if (testsignal) {
-    float signal = amplitude * (sin(CycleCounter * angularFrequency / afDivider) + phase);
-    Serial.println(signal);
-    analogWrite(analogpin, signal);
-    CycleCounter += 1;
+    testSignal();
+  }else{
+    readData();
   }
 }
 
+//test signal 
+void testSignal() {
+  float signal = amplitude * (sin(CycleCounter * angularFrequency / afDivider) + phase);
+  Serial.println(signal);
+  analogWrite(analogpin, signal);
+  CycleCounter += 1;
+}
+
+//Read the incomming singal form serial
 void readData() {
   while (Serial.available() > 0) {
-    interpretDataFlick();
+    if (useBumps)
+      interpretDataFlick();
+    else
+      interpretDataContinues();
   }
-  if(readyToRecvieSignal){
+  if (readyToRecvieSignal) {
     Serial.println("r");
   }
 }
 
+//Read the data and send a burst signal to the speaker
+//Inspred by https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
 void interpretDataFlick() {
-    readyToRecvieSignal = false;
-    in_Signal = Serial.readString().toInt();
-    analogWrite(analogpin, in_Signal);
-    delay(minimumDelay);
-    analogWrite(analogpin, 0);
-    Serial.flush();
-    readyToRecvieSignal = true;
+  readyToRecvieSignal = false;
+  in_Signal = Serial.readString().toInt();
+  analogWrite(analogpin, in_Signal);
+  delay(minimumDelay);
+  analogWrite(analogpin, 0);
+  Serial.flush();
+  readyToRecvieSignal = true;
 }
-
+//Read the data and send a continues signal to the speaker
+//Inspred by https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
 void interpretDataContinues() {
-    readyToRecvieSignal = false;
-    in_Signal = Serial.readString().toInt();
-    analogWrite(analogpin, in_Signal);
-    Serial.flush();
-    readyToRecvieSignal = true;
+  readyToRecvieSignal = false;
+  in_Signal = Serial.readString().toInt();
+  analogWrite(analogpin, in_Signal);
+  Serial.flush();
+  readyToRecvieSignal = true;
 }
--- ht/Mediap pipe 2.py
+++ ht/Mediap pipe 2.py
@@ -1,37 +1,40 @@
+#THE ORGINAL CODE CAME FROM: https://github.com/googlesamples/mediapipe/tree/main AND HAVE BEEN MODIFYEDE
 import cv2
 import mediapipe as mp
 import os
 import serial
-import matplotlib.pyplot as plt
 import numpy as np
-import time
+
+###*[PROGRAM START]###
 print("\n\n\n\n\n\n\n\n#######################PROGRAM STARTS#######################")
-##*Variables*##
+
+###*[VARIABLES]###
 print("Variables section started")
 serialportName = 'COM3'
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-folder = os.path.join(os.getcwd(),"HeighMap")
+folder = os.path.join(os.getcwd(), "HeighMap")
 heightmaps = os.listdir(folder)
-heighMap = cv2.imread(os.path.join(folder,heightmaps[0]), cv2.IMREAD_GRAYSCALE)
+heighMap = cv2.imread(os.path.join(folder, heightmaps[0]), cv2.IMREAD_GRAYSCALE)
 hmPreview = None
-resieHatMap = False
 signal = 0
 prevSignal = 0
-arduinoSerial = serial.Serial(port=serialportName,  baudrate=9600, timeout=.1)
+arduinoSerial = serial.Serial(port=serialportName, baudrate=9600, timeout=.1)
 isReadyToSend = True
-print("Variables Finnished")
-##**SETUP**##
+print("Variables Finished")
+
+###*[SETUP]###
 print("Setup Started")
-arduinoSerial.write(bytes(str(100),  'utf-8'))     # write a string
+arduinoSerial.write(bytes(str(100), 'utf-8'))  # write a string
 print(folder)
 print("Setup done")
-def write_read_serial(x,rts):
-    if(rts == True and x > 0):
-        print("The input signal"+ str(x))
-        arduinoSerial.write(bytes(str(x),  'utf-8'))
+
+def write_read_serial(x, rts):
+    if rts and x > 0:
+        print("The input signal" + str(x))
+        arduinoSerial.write(bytes(str(x), 'utf-8'))
         rts = False
     else:
         inputArd = arduinoSerial.readline().decode('utf-8').strip()
@@ -40,7 +43,7 @@ def write_read_serial(x,rts):
             rts = True
     return rts
 
-##**LOOP**##
+###*[LOOP]###
 print("Loop started")
 while True:
     success, image = cap.read()
@@ -48,43 +51,38 @@ while True:
     hmPreview = heighMap.copy()
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(imageRGB)
-    #lastPos = [0,0]
-        # checking whether a hand is detected
+
+    # checking whether a hand is detected
     if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks: # working with each hand
+        for handLms in results.multi_hand_landmarks:  # working with each hand
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                if id == 8 :
+                if id == 8:
                     try:
-                        heightValue = int(heighMap[cx,cy])
+                        heightValue = int(heighMap[cx, cy])
                         cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
                         cv2.circle(hmPreview, (cx, cy), 25, (heightValue, heightValue, heightValue), cv2.FILLED)
-                        print("cx: " + str(cx)+ " cy: " + str(cy))
-                        hhm = heighMap.shape[1]
-                        whm = heighMap.shape[0]
-                        print("hhm: " + str(hhm)+ " whm: " + str(whm))
-                        hi = image.shape[1]
-                        wi = image.shape[0]
-                        print("wi: " + str(wi)+ " hi: " + str(hi))
-                        finalX = int((cx/hi)*hhm)
-                        finalY = int((cy/wi)*whm)
-                        print("finalX: " + str(finalX)+ " finalY: " + str(finalY))
+                        print("cx: " + str(cx) + " cy: " + str(cy))
+                        hhm, whm = heighMap.shape
+                        print("hhm: " + str(hhm) + " whm: " + str(whm))
+                        hi, wi = image.shape[:2]
+                        print("wi: " + str(wi) + " hi: " + str(hi))
+                        finalX = int((cx / hi) * hhm)
+                        finalY = int((cy / wi) * whm)
+                        print("finalX: " + str(finalX) + " finalY: " + str(finalY))
                         prevSignal = signal
-                        signal = heighMap[finalX,finalY]
-                        
-                        print("signal"+ str(signal) + " The pump: " + str(abs(prevSignal-signal)))
-                        #print("cx: " + str(cx)+ " cy: " + str(cy) + " color value: " + str(heighMap[cx,cy]))
-                        
+                        signal = heighMap[finalX, finalY]
+                        print("signal" + str(signal) + " The pump: " + str(abs(prevSignal - signal)))
+
                     except:
                         print("An exception occurred")
-                    #print ("diaginal point 1 (x1,y1) = ({},{})".format(x1, y1))
-                    #print ("diaginal point 2 (x2,y2) = ({},{})".format(x2, y2))
 
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+    
     cv2.imshow("Input", image)
-    cv2.imshow("Output", hmPreview)     
-    isReadyToSend = write_read_serial(abs(prevSignal-signal),isReadyToSend)
+    cv2.imshow("Output", hmPreview)
+    isReadyToSend = write_read_serial(abs(prevSignal - signal), isReadyToSend)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Check for 'q' key press to exit
         break
